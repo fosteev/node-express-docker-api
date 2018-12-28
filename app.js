27854354ec0986d098e14b1a74ff771974043da0
Unknown <fosteev3@gmail.com>
2018-12-28 15:02:25 +0300

    Add headers

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json()); // for parsing application/json
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:7000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/containers', containersRouter);
 app.use('/images', imagesRouter);

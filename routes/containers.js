b35b36041e979568f308eafc913e54d5e4c9c5f0
Unknown <fosteev3@gmail.com>
2018-12-29 11:46:31 +0300

    Create containers

--- routes/containers.js
+++ routes/containers.js
@@ -4,25 +4,51 @@ var router = express.Router();
 'use strict';
 const {Docker} = require('node-docker-api');
 
-const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+const docker = new Docker({socketPath: '/var/run/docker.sock'});
 
 router.get('/', (req, res) => {
-    // List
-    // docker.container.list()
-    // // Inspect
-    //     .then(containers => containers[0].status())
-    //     .then(container => container.top())
-    //     .then(processes => console.log(processes))
-    //     .catch(error => console.log(error));
     let resContainers = [];
-    docker.container.list()
-    // Inspect
-        .then(containers => {
-            for (const key in containers) {
-                resContainers.push(containers[key].data);
-            }
-            res.json(resContainers);
-        })
+    docker.container.list().then(containers => {
+        for (const key in containers) {
+            resContainers.push(containers[key].data);
+        }
+        res.json(resContainers);
+    })
 });
+
+router.post('/create', (req, res) => {
+    const {id, image, container_port, exposed_port, name} = req.query;
+    const exposedPorts = {};
+
+    if (!(container_port && exposed_port && image, name)) {
+        res.status(403).send('Not have params');
+    }
+
+    const hostContainerPort = `${container_port}/tcp`;
+
+    exposedPorts[hostContainerPort] = [{
+        "HostPort": exposed_port.toString(),
+        "HostIp": "0"
+    }]
+
+    const params = {
+        Image: image,
+        name: name,
+        ExposedPorts: {},
+        HostPort: {
+            PortBindings: exposedPorts
+        }
+    };
+
+    params.ExposedPorts[hostContainerPort] = {};
+
+    docker.container.create(params)
+        .then(container => container.start())
+        .then(container => res.json(container.data))
+        .catch(error => {
+            const {json, statusCode} = error;
+            res.status(statusCode ? statusCode : 500).send(json)
+        });
+})
 
 module.exports = router;

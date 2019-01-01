bcbe8957e9210024efd04ba26c2df47ac400c10a
Unknown <fosteev3@gmail.com>
2019-01-01 16:45:01 -0500

    os view

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const os = require('os');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Web server run')
+    res.json({
+        EOL: os.EOL,
+        platform: os.platform(),
+        release: os.release(),
+        upTime: os.uptime(),
+        memory: (os.totalmem() / 10024 / 1024).toFixed(2),
+        homedir: os.homedir()
+})
 });
 
 module.exports = router;

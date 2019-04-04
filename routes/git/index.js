a48e414a9f42eac45e89cf1d7f1c56be37abf63e
Unknown <fosteev3@gmail.com>
2019-04-04 17:01:56 -0400

    delete set timeout

--- routes/git/index.js
+++ routes/git/index.js
@@ -69,30 +69,28 @@ router.get('/projects/:name', (req, res) => {
         const gitStatusText = await Settings.gitStatus(name);
         const branches = await Settings.gitBranches(name);
         const history = await Settings.getHistory(name);
-        setTimeout(() => {
-            res.json({
-                head: head.shorthand(),
-                changes: statuses.map(diffFile => diffFile.path()),
-                gitBranches: branches,
-                branch: {
-                    name: currentBranch.name(),
-                    owner: currentBranch.owner().state()
-                },
-                statusText: gitStatusText.split("\n").filter(item => item !== ""),
-                remotes: remotes.map(remote => remote),
-                remote: remote.connected(),
-                references: all,
-                branches: refs.map(ref => ref.isBranch()),
-                lastCommit: {
-                    author: lastCommit.author().toString(),
-                    message: lastCommit.message(),
-                    time: lastCommit.time(),
-                    date: lastCommit.date()
-                },
-                history: history.slice(0, 30)
-            })
-        }, 5000)
-;
+        
+        res.json({
+            head: head.shorthand(),
+            changes: statuses.map(diffFile => diffFile.path()),
+            gitBranches: branches,
+            branch: {
+                name: currentBranch.name(),
+                owner: currentBranch.owner().state()
+            },
+            statusText: gitStatusText.split("\n").filter(item => item !== ""),
+            remotes: remotes.map(remote => remote),
+            remote: remote.connected(),
+            references: all,
+            branches: refs.map(ref => ref.isBranch()),
+            lastCommit: {
+                author: lastCommit.author().toString(),
+                message: lastCommit.message(),
+                time: lastCommit.time(),
+                date: lastCommit.date()
+            },
+            history: history.slice(0, 30)
+        });
     })
 });
 
